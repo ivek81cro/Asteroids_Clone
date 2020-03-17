1d896e9e96f3c99a2eca3ca9f6238b7501957e86
ivek81cro <ivek81cro@gmail.com>
2020-03-17 18:37:29 +0100

    Resolve bullet rotation, texture death after initialization, rescale

--- Asteroids/Bullet.cpp
+++ Asteroids/Bullet.cpp
@@ -1,18 +1,16 @@
 #include "Bullet.h"
 #include "Constants.h"
 
-const float Bullet::lifetime = 1000.0f;
+const float Bullet::lifetime = 1.0f;
 const float Bullet::speed = 0.09f;
 
 Bullet::Bullet(sf::Vector2f position, float angle) :
 	is_alive(true), remaning_life(lifetime),
 	direction(cos(angle* DEGTORAD), sin(angle* DEGTORAD))
 {
-	tBullet.loadFromFile("images/fire_blue.png");
-	sBullet.setTexture(tBullet);
-	sBullet.setTextureRect(sf::IntRect(4,12,22,30));
-	//sBullet.setOrigin(135,2);
 	setPosition(position);
+	setRotation(angle + 90);
+	setScale(0.25f, 0.25f);
 }
 
 Bullet::~Bullet()
@@ -27,6 +25,10 @@ bool Bullet::isAlive()
 
 void Bullet::update(float frametime)
 {
+	tBullet.loadFromFile("images/fire_blue.png");
+	sBullet.setTexture(tBullet);
+	sBullet.setTextureRect(sf::IntRect(5, 0, 20, 55));
+	sBullet.setOrigin(27, 10);
 	if (!is_alive) return;
 
 	remaning_life = frametime;
@@ -34,6 +36,7 @@ void Bullet::update(float frametime)
 		is_alive = false;
 
 	sf::Vector2f distance = direction * speed * frametime;
+
 	move(distance);
 }
 
