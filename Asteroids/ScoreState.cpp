b7ac8471ac9ca55aadf020caecc2ea47246a8ce0
Ivan Batinić <ivek81cro@gmail.com>
2020-04-30 00:55:11 +0200

    Introduce ScoreState

---
+++ Asteroids/ScoreState.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "ScoreState.h"
+
+ScoreState::ScoreState(StateData* state_data)
+        : State(state_data)
+{
+}
+
+ScoreState::~ScoreState()
+{
+}
---
+++ Asteroids/ScoreState.h
@@ -0,0 +1,16 @@
+#ifndef SCORESTATE_H_
+#define SCORESTATE_H_
+
+#include "Button.h"
+#include "State.h"
+
+class ScoreState : public State
+{
+  public:
+    ScoreState(StateData* state_data);
+    virtual ~ScoreState();
+
+  private:
+
+};
+#endif
