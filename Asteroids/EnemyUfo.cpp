24f7dcac9a10deb8f7c4733681b7bf38e288bffc
Ivan Batinić <ivek81cro@gmail.com>
2020-05-10 00:11:11 +0200

    Fix lifteme start decrease point

--- Asteroids/EnemyUfo.cpp
+++ Asteroids/EnemyUfo.cpp
@@ -5,8 +5,8 @@ EnemyUfo::EnemyUfo(float x, float y, sf::Texture& texture_sheet, float scale)
         : invoulnerability_(true)
         , away_(false)
         , points_(40)
-        , lifetime_(10.f)
-        , fire_cooldown_(5.f)
+        , lifetime_(6.f)
+        , fire_cooldown_(1.f)
 {
     scale_factor_ = scale;
     sprite_.setScale(scale_factor_, scale_factor_);
@@ -28,27 +28,26 @@ void EnemyUfo::InitComponents(sf::Texture& texture_sheet)
 {
     CreateHitboxComponent(sprite_, 45.f * scale_factor_);
     CreateAnimationComponent(texture_sheet);
+    CreateMovementComponent(0.f, 0.f, 0.f, sprite_.getRotation());
 
     animation_component_->AddAnimation("enemy_ufo", 9.f, 0, 0, 17, 0, 48, 48);
     animation_component_->AddAnimation("enemy_ufo_active", 3.f, 0, 1, 5, 1, 48, 48);
-    animation_component_->AddAnimation("enemy_ufo_away", 6.f, 11, 1, 17, 1, 48, 48);
+    animation_component_->AddAnimation("enemy_ufo_away", 9.f, 0, 1, 17, 1, 48, 48);
     animation_component_->AddAnimation("enemy_ufo_exploding", 5.f, 0, 2, 16, 2, 48, 48);
 }
 
 void EnemyUfo::Update(const float& delta, const sf::Vector2u& window_size)
 {
-    if (invoulnerability_)
+    if (invoulnerability_ && !away_)
     {
         if (animation_component_->Play(animation_name_, delta, true))
         {
-            invoulnerability_       = false;
-            animation_name_ = "enemy_ufo_active";
+            invoulnerability_ = false;
+            animation_name_   = "enemy_ufo_active";
         }
     }
-    if (away_)
+    if (away_ && invoulnerability_)
     {
-        invoulnerability_ = true;
-        fire_cooldown_    = 20.f;
         if (animation_component_->Play(animation_name_, delta, true))
         {
             away_          = false;
@@ -63,7 +62,7 @@ void EnemyUfo::Update(const float& delta, const sf::Vector2u& window_size)
         }
     }
     else
-        animation_component_->Play(animation_name_, delta);
+        Entity::Update(delta, window_size);
 
     std::cout << lifetime_ << std::endl;
 }
@@ -85,12 +84,17 @@ void EnemyUfo::ResetFireCooldown()
 
 void EnemyUfo::SetLifeTime(const float& delta)
 {
-    lifetime_ -= delta;
-    fire_cooldown_ -= delta;
+    if (!invoulnerability_)
+    {
+        lifetime_ -= delta;
+        fire_cooldown_ -= delta;
+    }
+
     if (lifetime_ < 0)
     {
-        animation_name_ = "enemy_ufo_away";
-        away_ = true;
+        animation_name_   = "enemy_ufo_away";
+        away_             = true;
+        invoulnerability_ = true;
     }
 }
 
